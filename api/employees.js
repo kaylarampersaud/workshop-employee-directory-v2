8b75f6328592bcb9937aed811ffb55aa7710c940
Kayla Rampersaud <krampersaud98@gmail.com>
2026-02-18 12:41:23 -0500

    added helper functions to database employees and added POST /employee with 400 and 201 status

--- api/employees.js
+++ api/employees.js
@@ -12,6 +12,16 @@ router.get("/", (req, res) => {
   res.send(employees);
 });
 
+router.post("/", (req, res) => {
+  if (!req.body) return res.status(400).send("Request body is required.");
+
+  const { name } = req.body;
+  if (!name) return res.status(400).send("Name is required.");
+
+  const employee = addEmployee(name);
+  res.status(201).send(employee);
+});
+
 router.get("/random", (req, res) => {
   const employee = getRandomEmployee();
   res.send(employee);
--- db/employees.js
+++ db/employees.js
@@ -11,5 +11,34 @@ const employees = [
   { id: 10, name: "Gwen Grollmann" },
 ];
 
+/**
+ * Creates a new employee and adds into the array.
+ * @param {string} name - the name of the new employee
+ * @returns {Employee} - the newly created employee
+ */
+
+export function addEmployee(name) {
+  const employee = { id: employees.length + 1, name };
+  employees.push(employee);
+  return employee;
+}
+
+/**
+ * @param {number} id - ID of the employee to retrieve
+ * @returns {Employee} the employee with the given ID
+ * @returns {undefined} if no employee with the given ID exists
+ */
+export function getEmployeeById(id) {
+  return employees.find((e) => e.id === id);
+}
+
+/**
+ * @returns {Employee} a random employee
+ */
+export function getRandomEmployee() {
+  const randomIndex = Math.floor(Math.random() * employees.length);
+  return employees[randomIndex];
+}
+
 /* WARNING: this must remain the default export in order for the tests to work! */
 export default employees;
